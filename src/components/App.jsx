10bbf87d9b0a78eb758bae2534f96e96641d34be
AndreyPokotilo <102250174+AndreyPokotilo@users.noreply.github.com>
2023-05-20 14:26:18 +0200

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,9 @@
+// import axios from 'axios';
+// import PropTypes from 'prop-types';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <div>
       React homework template
     </div>
   );
